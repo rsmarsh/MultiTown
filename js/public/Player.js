7c8dbad4263877dec22a8830516f93a3d071bbe3
unknown <missingmarshall@gmail.com>
2019-06-09 12:44:54 +0100

    Added player spawn, game engine implementations

--- js/public/Player.js
+++ js/public/Player.js
@@ -3,23 +3,119 @@ import * as utils from './utilities.js';
 class Player {
     constructor(name, config) {
         this.name = name;
-        this.graphics = config.graphics;
+        this.config = config;
+        // this.graphics = config.graphics;
+        
         for (var property in config){
             this[property] = config[property];
         }
 
-        this.createSprite(this.colour);
+        this.addGraphics();
+        this.addPhysics();
+
+        if (this.controllable) {
+            this.addControls();
+        }
     };
+
+    addPhysics() {
+        this.player.setBounce(1);
+        this.player.setCollideWorldBounds(true);
+        
+        // world objects which the player model should interact with
+        for (var i = 0; i < this.config.collideWith.length; i++) {
+            this.scene.physics.add.collider(this.player, this.config.collideWith[i]);
+        }
+        // this.player.body.setGravityY(100);
+    };
+    
+    addGraphics() {
+        this.player = this.scene.physics.add.sprite(100, 450, 'dude');
+        this.scene.anims.create({
+            key: 'left',
+            frames: this.scene.anims.generateFrameNumbers('dude', {
+                start: 0,
+                end: 0
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.scene.anims.create({
+            key: 'right',
+            frames: this.scene.anims.generateFrameNumbers('dude', {
+                start: 5,
+                end: 8
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        
+        this.scene.anims.create({
+            key: 'turn',
+            frames: [{
+                key: 'dude',
+                frame: 4
+            }],
+            frameRate: 20
+        })
+
+    }
 
     // creates the onscreen spite to be used by this player
     createSprite(colour) {
-        this.colour = this.colour || utils.getRandomColour();
-        var rect = new Phaser.Geom.Rectangle(50, 50, 50, 50);
+        // this.colour = this.colour || utils.getRandomColour();
+        // var rect = new Phaser.Geom.Rectangle(50, 50, 50, 50);
 
-        this.graphics.fillStyle(this.colour);
-        this.graphics.fillRectShape(rect);
+        // this.graphics.fillStyle(this.colour);
+        // this.graphics.fillRectShape(rect);
+        
+
 
     };
-};
+
+    addControls() {
+        this.movementKeys = {
+            'up': this.scene.input.keyboard.addKey('W'),
+            'down': this.scene.input.keyboard.addKey('S'),
+            'left': this.scene.input.keyboard.addKey('A'),
+            'right': this.scene.input.keyboard.addKey('D'),
+            getKey: () => {
+                // iterate over each key object and return the first result
+                for (var key in this.movementKeys) {
+                    if (this.movementKeys[key].isDown) {
+                        return key;
+                    }
+                }
+            }
+        };
+
+
+    };
+
+    // called each frame by the game's update loop
+    updatePosition() {
+        var keyPressed = this.movementKeys.getKey();
+
+        if (!keyPressed) {
+            return;
+        }
+
+        switch(keyPressed) {
+            case 'up':
+                console.log("going up");
+                break;
+            case 'down':
+                console.log("going down");
+                break;
+            case 'left':
+                console.log("going left");
+            break;
+            case 'right':
+                console.log("going right");
+            break;
+        }
+
+    };
+}
 
 export default Player;
--- js/public/game.js
+++ js/public/game.js
@@ -4,6 +4,13 @@ var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {y: 300},
+      debug: false
+    }
+  },
   scene: {
     preload: preload,
     create: create,
@@ -12,23 +19,51 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var currentPlayer;
 
 function preload() {
-  // this.load.image('imageName', 'assets/*.png');
+  this.load.image('sky', 'assets/env/sky.png');
+  this.load.image('ground', 'assets/env/platform.png');
+  this.load.image('star', 'assets/env/star.png');
+  this.load.image('bomb', 'assets/env/bomb.png');
+  this.load.spritesheet('dude', 
+        'assets/player/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
 }
 
+var platforms;
+
 function create() {
-  // this.add.image(400, 300, 'imageName');
+  this.add.image(400, 300, 'sky');
+  
+  platforms = this.physics.add.staticGroup();
+  
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
+
+  
   addPlayer.call(this);
 }
 
 function update() {
 
+  // checks for keyboard inputs and moves the player if the necessary keys are down
+  currentPlayer.updatePosition();
 }
 
 
 function addPlayer(){
+  var sceneRef = this;
   var graphics = this.add.graphics();
-  var player1 = new Player('richard', {graphics: graphics});
+  currentPlayer = new Player('richard', {
+    scene: sceneRef,
+    graphics: graphics,
+    controllable: true,
+    collideWith: [platforms]
+  });
+  
 
 }
